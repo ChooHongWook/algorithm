c7db3bc879c4ea5e556bf29c4d3188e8b186f481
olt13mm <olt13mm@gmail.com>
2025-03-23 14:54:54 +0900

    Add: Optimize 4.findMedianSortedArrays.js

--- basic/4.findMedianSortedArrays.js
+++ basic/4.findMedianSortedArrays.js
@@ -54,3 +54,58 @@ var findMedianSortedArrays = function (nums1, nums2) {
     ? halfMergedArray.at(-1)
     : (halfMergedArray.at(-1) + halfMergedArray.at(-2)) / 2;
 };
+
+/**
+ * Optimize the space complexity
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays2 = function (nums1, nums2) {
+  /**
+   * 1. 정렬된 배열 2개의 전체 길이를 찾는다.
+   * 2. 배열을 전체의 절반 길이까지 순서만 찾는다
+   * 3. 홀수면 마지막 1개 짝수면 마지막 2개의 값을 가져와서 중앙값을 찾는다
+   *
+   * 1. Find the total length of the two sorted arrays.
+   * 2. Identify the elements up to half the total length of the arrays.
+   * 3. Take the last num if the total length is odd, or the last two nums if even, to find the median.
+   */
+
+  const allLength = nums1.length + nums2.length;
+  const allLengthmeidan = Math.floor(allLength / 2);
+
+  const isOdd = !!(allLength % 2);
+
+  let idx1 = 0;
+  let idx2 = 0;
+
+  let curData, beforeData;
+  for (let i = 0; i <= allLengthmeidan; i++) {
+    beforeData = curData;
+    const t1 = nums1[idx1];
+    const t2 = nums2[idx2];
+    if (typeof t2 !== 'number') {
+      curData = t1;
+      idx1++;
+      continue;
+    }
+    if (typeof t1 !== 'number') {
+      curData = t2;
+      idx2++;
+      continue;
+    }
+    if (t1 <= t2) {
+      curData = t1;
+      idx1++;
+      continue;
+    }
+    if (t1 >= t2) {
+      curData = t2;
+      idx2++;
+      continue;
+    }
+  }
+
+  return isOdd ? curData : (curData + beforeData) / 2;
+};
