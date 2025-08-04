bc8814a3ac8a078101773906eab53a01d8e07b6f
Hong Wook Choo <hw.choo@amicuslex.net>
2025-08-04 23:44:23 +0900

    Fix: 11.container-with-most-water (format and link)

--- basic/11.container-with-most-water.js
+++ basic/11.container-with-most-water.js
@@ -1,23 +1,23 @@
-// https://leetcode.com/problems/regular-expression-matching/
+// https://leetcode.com/problems/container-with-most-water/
 
 /**
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    let result = 0  
-    for(let idx1 = 0; idx1 < height.length-1; idx1++) {
-        for(let idx2 = idx1+1; idx2 < height.length; idx2++) {
-            let h1 = height[idx1]
-            let h2 = height[idx2]
+var maxArea = function (height) {
+  let result = 0;
+  for (let idx1 = 0; idx1 < height.length - 1; idx1++) {
+    for (let idx2 = idx1 + 1; idx2 < height.length; idx2++) {
+      let h1 = height[idx1];
+      let h2 = height[idx2];
 
-            let h = Math.min(h1, h2)
+      let h = Math.min(h1, h2);
 
-            let area = h * (idx2 - idx1)
-            if(result < area) result = area
-        }
-    } 
-    return result
+      let area = h * (idx2 - idx1);
+      if (result < area) result = area;
+    }
+  }
+  return result;
 };
 
 /**
@@ -25,16 +25,16 @@ var maxArea = function(height) {
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    let result = 0
-    let left = 0
-    let right = height.length-1
+var maxArea = function (height) {
+  let result = 0;
+  let left = 0;
+  let right = height.length - 1;
 
-    while (left < right) {
-        let area = Math.min(height[left], height[right]) * (right - left)
-        if (area > result)result = area
-        height[left] <= height[right] ? left++ : right--
-    }
-    
-    return result
+  while (left < right) {
+    let area = Math.min(height[left], height[right]) * (right - left);
+    if (area > result) result = area;
+    height[left] <= height[right] ? left++ : right--;
+  }
+
+  return result;
 };
